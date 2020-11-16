7d0df62a471e442981c630598c7bca3054b94ebf
Đạt <huydat26021999@gmail.com>
2020-11-17 00:38:39 +0700

    huydat69

---
+++ DAL/Interfaces/IKhachHangRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Model;
+
+namespace DAL
+{
+   public interface IKhachHangRepository
+    {
+        List<KhachHangModel> GetKh();
+        KhachHangModel getbyid(string id);
+        List<DiaChiModel> GeDiachi(string id);
+        TaiKhoanModel GetTaiKhoan(string makh);
+    }
+}
---
+++ Model/LoaiModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class LoaiModel
+    {
+        public int MaLoai { get; set; }
+
+        public string TenLoai { get; set; }
+
+        public string MoTa { get; set; }
+
+        public string GhiChu { get; set; }
+
+        public string link { get; set; }
+
+        public int? removed { get; set; }
+
+        public int? displayed { get; set; }
+        public List<LoaiCon1Model> children { get; set; }
+
+    }
+}
